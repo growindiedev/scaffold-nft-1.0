4f05bc7a93c58d32f594d09da90c65b9b524bc8b
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-01-27 05:21:57 +0530

    updated etherscan key

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,8 +1,6 @@
-// MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
 export const INFURA_ID = "921d109adcf6424fb023d614cfffd209";
 
-// MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
-export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
+export const ETHERSCAN_KEY = "W8KQNX9XA34DBZMX6WC88S1M1I7PYU9IQA";
 
 // BLOCKNATIVE ID FOR Notify.js:
 export const BLOCKNATIVE_DAPPID = "0b58206a-f3c0-4701-a62f-73c7243e8c77";
